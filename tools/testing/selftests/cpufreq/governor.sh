57bcca69288b9ac83c986e71278cb124d72336e3
Daniel Hodges <hodges.daniel.scott@gmail.com>
2024-03-06 21:03:44 -0500

    Add selftest for bpf cpufreq
    Add additional self test for bpf cpufreq governor.

--- tools/testing/selftests/cpufreq/governor.sh
+++ tools/testing/selftests/cpufreq/governor.sh
@@ -110,7 +110,7 @@ switch_show_governor()
 
 	printf "\nSwitched governor for $1 to $2\n\n"
 
-	if [ $2 == "userspace" -o $2 == "powersave" -o $2 == "performance" ]; then
+	if [ $2 == "userspace" -o $2 == "powersave" -o $2 == "performance" -o $w == "bpf" ]; then
 		printf "No files to read for $2 governor\n\n"
 		return
 	fi
